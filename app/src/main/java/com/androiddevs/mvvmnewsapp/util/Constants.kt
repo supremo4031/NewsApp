821d485425278979915c61561b6cbea5f301fe2c
polygonsolution <66715995+polygonsolution@users.noreply.github.com>
2020-12-28 15:37:16 +0530

    retrofit added

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "06047443f4bb43ca999cc5ebc1dd9152"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
