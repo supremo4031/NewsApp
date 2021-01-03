de5e53fea9b3e46f8edb441fcb7c4ce3afd8f225
polygonsolution <66715995+polygonsolution@users.noreply.github.com>
2021-01-03 14:43:31 +0530

    handling breaking news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapters.NewsAdapter
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class NewsActivity : AppCompatActivity() {
 
@@ -25,4 +28,5 @@ class NewsActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
 
     }
+
 }
